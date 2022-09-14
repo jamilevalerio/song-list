b265d02cc915f247dbfdb822d9218146fc81cd59
Jamile Valério <jamilevalerio.code@gmail.com>
2022-09-13 23:11:22 -0300

    Adding an input to artist's name, updating the code to get the input's value and manipulating dinamically the HTML.

--- script.js
+++ script.js
@@ -22,23 +22,27 @@ const songList = {
         artist: dados.artist,
         song: dados.song
     })
+
     //creates post on HTML
     const songItem = document.querySelector('.song-list');
-    songItem.insertAdjacentHTML("afterbegin", `<li>${dados.song}</li>`);
+    songItem.insertAdjacentHTML("afterbegin", `<li>${dados.song  + ' - ' + dados.artist}</li>`);
     //insertAdjacentHTML is used to insert elements on the webpage
   },
 };
-songList.createList({artist: 'beyoncé' , song: 'haunted'});
-console.log(songList.posts);
 
 const musicForm = document.querySelector('form'); //gets the HTML element
 
 //gets the submission
 musicForm.addEventListener('submit', function musicPostController(eventInfo){
   eventInfo.preventDefault(); //'preventDefault' stops the reload default event
-  
-  const songName = document.querySelector('.song-name');
 
-  songName.value = ''; //clears the input after the value is submited
+  const songName = document.querySelector('.song-name');
+  const artistName = document.querySelector('.artist-name');
+
+  songList.createList({artist: songName.value, song: artistName.value});
+
+  //clears the input after the value is submited
+  songName.value = '';
+  artistName.value = '';
 })
 
